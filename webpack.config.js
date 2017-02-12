368598579881a24aa9b7a55999df9c9530620a9e
Francesco Bonomi <frbonomi@gmail.com>
2017-02-12 19:01:54 +0100

    Add sass and js loaders for webapack

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      loaders: ["style-loader", "css-loader", "sass-loader"]
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      options: { presets: ['es2015'] },
+      exclude: path.resolve(__dirname, "node_modules")
+    }]
   }
 };
