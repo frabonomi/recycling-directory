cd73a10e5a2f8c8ae46d1b3421a75799bbd5db24
Francesco Bonomi <frbonomi@gmail.com>
2017-02-19 00:52:57 +0100

    Filter results by query string

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,20 @@
+import results from '../data/results.json';
 import ResultsList from './results-list/results-list.vue';
 
 export default {
   name: 'app',
   template: '#app',
   components: { ResultsList },
-  data() {
+  data: () => {
     return {
+      results: results,
       query: '',
     }
-  }
+  },
+  watch: {
+    query: function(query) {
+      this.results = results.filter(
+        (el) => _.includes(el.name.toLowerCase(), query.toLowerCase()));
+    }
+  },
 }
--- app/components/results-list/results-list.js
+++ app/components/results-list/results-list.js
@@ -1,5 +1,5 @@
 export default {
   name: 'results-list',
   template: '#results-list',
-  props: ['query'],
+  props: ['results'],
 }
